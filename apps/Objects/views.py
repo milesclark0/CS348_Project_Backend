c3fa339489904c125239d4d04a12992b82c0d417
divysunderraj <divysunderraj@gmail.com>
2022-12-05 21:21:58 -0500

    items are pulled from stored procedure now

--- apps/Objects/views.py
+++ apps/Objects/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from apps.Objects.models import Item
+from django.db import connection
 
 # Create your views here.
 def show_items(request):
-    items = Item.objects.all()
-    return render(request,"show_items.html",{'items':items})
+    cursor = connection.cursor()
+    cursor.execute("call GetAllItems()")
+    results = cursor.fetchall()
+    return render(request, "show_items.html", {'items': results})
+    #items = Item.objects.all()
+    #return render(request,"show_items.html",{'items':items})
